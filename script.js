2af652e1f86653edfbbbb9d2c742b3fcec181d20
hellen-Tchovelidze <elenechovelidze@gmail.com>
2024-11-17 13:25:37 +0400

    js

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 let button = document.getElementById("button");
 let productContainer = document.getElementById("product-container");
-let productId = 1
- async function SaleControl()  {
- try {
-    let Sale = await fetch("https://fakestoreapi.com/products/${productId}")
+let productId = 1;
+
+async function SaleControl() {
+  try {
+    let Sale = await fetch(`https://fakestoreapi.com/products/${productId}`);
     let data = await Sale.json();
-    data.map(()=>{
-        let productInfo = document.createElement("p");
-        productInfo.textContent = data.price;
-        button.appendChild(productInfo);
-        productId++;
-    })
-   
- } catch (error) {
+    let productInfo = document.createElement("p");
+    productInfo.textContent = `Product Name: ${data.title}, Price: $${data.price}`;
+    button.appendChild(productInfo);
+    productId++;
+  } catch (error) {
     let errorMessage = document.createElement("p");
-        errorMessage.textContent = `Error: ${error.message}`;
-        productContainer.appendChild(errorMessage);
- }}
-    
-
-
-SaleControl()
+    errorMessage.textContent = `არასწორი ღილაკი`;
+    productContainer.appendChild(errorMessage);
+  }
+}
 
 button.addEventListener("click", () => {
-    fetchProduct(productId);
-    productId++; 
+  SaleControl();
+  productId++;
 });
